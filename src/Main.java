7df57a7210429ceca55d6a4bb173dd79d4e2de5a
Elif Meral <eliral.mrl67@gmail.com>
2025-05-13 08:51:28 +0200

    Commit pour pouvoir push

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import donjon.Donjon;
 import personnages.Joueur;
 import personnages.classes.Guerrier;
 import personnages.races.Humain;
@@ -8,5 +9,8 @@ public class Main {
         System.out.println("Bienvenue dans DOOnjon et Dragons");
         Joueur Diego = new Joueur("Diego", new Humain(), new Guerrier());
         System.out.println(Diego.toString());
+        Donjon d = new Donjon(15,15);
+        d.remplir();
+        System.out.println(d.m_plateau[0][0]);
     }
 }
