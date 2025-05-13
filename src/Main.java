e67f7a23db1d9a7cac00ab0a55528fe7e0a1f807
Diego Aparicio <diego.aparicio@etu.unistra.fr>
2025-05-13 17:31:38 +0200

    Ajout de la classe Jeu, modification de la classe Joueur et la classe Donjon.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
-import donjon.Donjon;
-import personnages.Joueur;
-import personnages.classes.Guerrier;
-import personnages.races.Humain;
+import jeu.Jeu;
 
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Bienvenue dans DOOnjon et Dragons");
-        Joueur Diego = new Joueur("Diego", new Humain(), new Guerrier());
-        System.out.println(Diego.toString());
-        Donjon d = new Donjon(15,15);
-        d.remplir();
-        System.out.println(d.m_plateau[0][0]);
+        Jeu partie = new Jeu();
+        partie.creerJoueur();
     }
 }
