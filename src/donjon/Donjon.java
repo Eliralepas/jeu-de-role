7ad4a0f72bd619d1c968d6d0267b8d6cc632c17f
Diego Aparicio <diego.aparicio@etu.unistra.fr>
2025-05-13 11:30:23 +0200

    Création d'une classe Pion et ajout de la logique de gestion du pion dans le donjon

--- src/donjon/Donjon.java
+++ src/donjon/Donjon.java
@@ -1,7 +1,12 @@
 package donjon;
 
+import donjon.pions.Pion;
+
+import java.util.ArrayList;
+
 public class Donjon {
-    public String[][] m_plateau;
+    private String[][] m_plateau;
+    private ArrayList<Pion> m_pions;
     private int m_longueur;
     private int m_largeur;
 
@@ -9,6 +14,7 @@ public class Donjon {
         m_longueur = longueur;
         m_largeur = largeur;
         m_plateau = new String[longueur][largeur];
+        m_pions = new ArrayList<Pion>();
     }
 
     public void remplir(){
@@ -19,9 +25,19 @@ public class Donjon {
         }
     }
 
-    public void ajouter(String symbol){
-        
+    public void ajouterPion(Pion p){
+        m_pions.add(p);
     }
 
+    public void supprimerPion(Pion p){
+        m_pions.remove(p);
+    }
 
+    private void afficherPions(){
+        for(Pion p: m_pions){
+            int x = p.getX();
+            int y = p.getY();
+            m_plateau[x][y] = p.toString();
+        }
+    }
 }
