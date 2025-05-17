d798a26a60c196a9ec6bd7dc4f04418973fe8387
Elif Meral <eliral.mrl67@gmail.com>
2025-05-17 23:33:01 +0200

    Ajout de test

---
+++ tests/personnages/JoueurTest.java
@@ -0,0 +1,11 @@
+package personnages;
+
+import personnages.*;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JoueurTest {
+    @Test
+    void testConstructeur() {}
+}
---
+++ tests/personnages/PersonnageTest.java
@@ -0,0 +1,11 @@
+package personnages;
+
+import org.junit.jupiter.api.Test;
+import personnages.Personnage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonnageTest {
+    @Test
+    void testConstructeur() {}
+}
